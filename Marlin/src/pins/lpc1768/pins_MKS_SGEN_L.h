10faf24faeb00d6ad20bfa32808b10698a073638
itthings <44327454+itthings@users.noreply.github.com>
2021-07-07 23:51:46 +0100

    my-2.0.8.9

--- Marlin/src/pins/lpc1768/pins_MKS_SGEN_L.h
+++ Marlin/src/pins/lpc1768/pins_MKS_SGEN_L.h
@@ -25,9 +25,7 @@
  * MKS SGEN-L pin assignments
  */
 
-#if NOT_TARGET(MCU_LPC1768)
-  #error "Oops! Make sure you have the LPC1768 environment selected in your IDE."
-#endif
+#include "env_validate.h"
 
 #define BOARD_INFO_NAME   "MKS SGen-L"
 #define BOARD_WEBSITE_URL "github.com/makerbase-mks/MKS-SGEN_L"
@@ -43,16 +41,16 @@
 //
 #define X_DIAG_PIN                         P1_29  // X-
 #define Y_DIAG_PIN                         P1_27  // Y-
-#define Z_DIAG_PIN                         P1_25  // Z-
+#define Z_DIAG_PIN                      P1_26  // P1_25  // Z-
 #define E0_DIAG_PIN                        P1_28  // X+
-#define E1_DIAG_PIN                        P1_26  // Y+
+#define E1_DIAG_PIN                    P1_25 //    P1_26  // Y+
 
 //
 // Limit Switches
 //
 #ifdef X_STALL_SENSITIVITY
   #define X_STOP_PIN                  X_DIAG_PIN
-  #if X_HOME_DIR < 0
+  #if X_HOME_TO_MIN
     #define X_MAX_PIN                      P1_28  // X+
   #else
     #define X_MIN_PIN                      P1_28  // X+
@@ -64,7 +62,7 @@
 
 #ifdef Y_STALL_SENSITIVITY
   #define Y_STOP_PIN                  Y_DIAG_PIN
-  #if Y_HOME_DIR < 0
+  #if Y_HOME_TO_MIN
     #define Y_MAX_PIN                      P1_26  // Y+
   #else
     #define Y_MIN_PIN                      P1_26  // Y+
@@ -76,7 +74,7 @@
 
 #ifdef Z_STALL_SENSITIVITY
   #define Z_STOP_PIN                  Z_DIAG_PIN
-  #if Z_HOME_DIR < 0
+  #if Z_HOME_TO_MIN
     #define Z_MAX_PIN                      P1_24  // Z+
   #else
     #define Z_MIN_PIN                      P1_24  // Z+
@@ -110,12 +108,21 @@
   #define Y_CS_PIN                         P1_08
 #endif
 
-#define Z_STEP_PIN                         P0_22
-#define Z_DIR_PIN                          P2_11
-#define Z_ENABLE_PIN                       P0_21
-#ifndef Z_CS_PIN
-  #define Z_CS_PIN                         P1_10
+#define E1_STEP_PIN                         P0_22
+#define E1_DIR_PIN                          P2_11
+#define E1_ENABLE_PIN                       P0_21
+#ifndef E1_CS_PIN
+  #define E1_CS_PIN                         P1_10
 #endif
+
+#define Z_STEP_PIN                     P0_01//    P0_22
+#define Z_DIR_PIN                      P0_00//    P2_11
+#define Z_ENABLE_PIN                   P0_10//    P0_21
+#ifndef Z_CS_PIN
+  #define Z_CS_PIN                     P1_17//    P1_10
+#endif
+
+
 
 #define E0_STEP_PIN                        P2_13
 #define E0_DIR_PIN                         P0_11
@@ -124,13 +131,12 @@
   #define E0_CS_PIN                        P1_15
 #endif
 
-#define E1_STEP_PIN                        P0_01
-#define E1_DIR_PIN                         P0_00
-#define E1_ENABLE_PIN                      P0_10
+#define E1_STEP_PIN                   P0_22  //   P0_01
+#define E1_DIR_PIN                    P2_11  //   P0_00
+#define E1_ENABLE_PIN                 P0_21 //    P0_10
 #ifndef E1_CS_PIN
-  #define E1_CS_PIN                        P1_17
+  #define E1_CS_PIN                   P1_10  //   P1_17
 #endif
-
 //
 // Software SPI pins for TMC2130 stepper drivers
 //
@@ -175,29 +181,29 @@
   #define Y_SERIAL_TX_PIN                  P1_09
   #define Y_SERIAL_RX_PIN                  P1_08
 
-  #define Z_SERIAL_TX_PIN                  P1_14
-  #define Z_SERIAL_RX_PIN                  P1_10
+  #define Z_SERIAL_TX_PIN              P4_29  //  P1_14
+  #define Z_SERIAL_RX_PIN              P1_17//    P1_10
 
   #define E0_SERIAL_TX_PIN                 P1_16
   #define E0_SERIAL_RX_PIN                 P1_15
 
-  #define E1_SERIAL_TX_PIN                 P4_29
-  #define E1_SERIAL_RX_PIN                 P1_17
+  #define E1_SERIAL_TX_PIN            P1_14    // P4_29
+  #define E1_SERIAL_RX_PIN            P1_10   //  P1_17
 
   #define Z2_SERIAL_TX_PIN                 P4_29
   #define Z2_SERIAL_RX_PIN                 P1_17
 
   // Reduce baud rate to improve software serial reliability
   #define TMC_BAUD_RATE                    19200
-#endif // TMC2208 || TMC2209
+#endif // HAS_TMC_UART
 
 //
 // Temperature Sensors
 // 3.3V max when defined as an analog input
 //
 #define TEMP_0_PIN                      P0_23_A0  // Analog Input A0 (TH1)
-#define TEMP_BED_PIN                    P0_24_A1  // Analog Input A1 (TB)
-#define TEMP_1_PIN                      P0_25_A2  // Analog Input A2 (TH2)
+#define TEMP_BED_PIN                P0_25_A2 //    P0_24_A1  // Analog Input A1 (TB)
+#define TEMP_1_PIN                  P0_24_A1  //    P0_25_A2  // Analog Input A2 (TH2)
 
 //
 // Heaters / Fans
@@ -229,7 +235,7 @@
  *                _____                                            _____
  * (BEEPER) 1.31 | · · | 1.30 (BTN_ENC)          (MISO)       0.8 | · · | 0.7  (SD_SCK)
  * (LCD_EN) 0.18 | · · | 0.16 (LCD_RS)           (BTN_EN1)   3.25 | · · | 0.28 (SD_CS2)
- * (LCD_D4) 0.15 | · · | 0.17 (LCD_D5)           (BTN_EN2)   3.26 | · · | 0.9  (SD_MOSI)
+ * (LCD_D4) 0.15 | · ·   0.17 (LCD_D5)           (BTN_EN2)   3.26 | · ·   0.9  (SD_MOSI)
  * (LCD_D6)  1.0 | · · | 1.22 (LCD_D7)           (SD_DETECT) 0.27 | · · | RST
  *           GND | · · | 5V                                   GND | · · | NC
  *                -----                                            -----
@@ -248,6 +254,35 @@
 
     #define LCD_PINS_ENABLE                P1_22
     #define LCD_PINS_D4                    P0_17
+
+  #elif HAS_SPI_TFT                               // Config for Classic UI (emulated DOGM) and Color UI
+    #define TFT_CS_PIN                     P1_00
+    #define TFT_A0_PIN                     P1_22
+    #define TFT_DC_PIN                     P1_22
+    #define TFT_MISO_PIN                   P0_08
+    #define TFT_BACKLIGHT_PIN              P0_18
+    #define TFT_RESET_PIN                  P0_16
+
+    #define LCD_USE_DMA_SPI
+
+    #define TOUCH_INT_PIN                  P0_17
+    #define TOUCH_CS_PIN                   P0_15
+    #define TOUCH_BUTTONS_HW_SPI
+    #define TOUCH_BUTTONS_HW_SPI_DEVICE        2
+
+    // Disable any LCD related PINs config
+    #define LCD_PINS_ENABLE                -1
+    #define LCD_PINS_RS                    -1
+
+    #ifndef TFT_BUFFER_SIZE
+      #define TFT_BUFFER_SIZE               1200
+    #endif
+    #ifndef TFT_QUEUE_SIZE
+      #define TFT_QUEUE_SIZE                6144
+    #endif
+
+    #define BTN_EN1                        P3_25
+    #define BTN_EN2                        P3_26
 
   #elif IS_TFTGLCD_PANEL
 
@@ -319,10 +354,15 @@
           #define DOGLCD_A0                P1_00
         #endif
 
-        #if ENABLED(ULTIPANEL)
+        #if IS_ULTIPANEL
           #define LCD_PINS_D5              P0_17
           #define LCD_PINS_D6              P1_00
           #define LCD_PINS_D7              P1_22
+
+          #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
+            #define BTN_ENC_EN       LCD_PINS_D7  // Detect the presence of the encoder
+          #endif
+
         #endif
 
       #endif // !FYSETC_MINI_12864
@@ -341,13 +381,13 @@
 
 #if SD_CONNECTION_IS(LCD) || SD_CONNECTION_IS(ONBOARD)
   #define SD_DETECT_PIN                    P0_27
-  #define SCK_PIN                          P0_07
-  #define MISO_PIN                         P0_08
-  #define MOSI_PIN                         P0_09
+  #define SD_SCK_PIN                       P0_07
+  #define SD_MISO_PIN                      P0_08
+  #define SD_MOSI_PIN                      P0_09
   #if SD_CONNECTION_IS(ONBOARD)
-    #define SS_PIN             ONBOARD_SD_CS_PIN
+    #define SD_SS_PIN          ONBOARD_SD_CS_PIN
   #else
-    #define SS_PIN                         P0_28
+    #define SD_SS_PIN                      P0_28
   #endif
 #elif SD_CONNECTION_IS(CUSTOM_CABLE)
   #error "No custom SD drive cable defined for this board."
@@ -358,4 +398,4 @@
 //
 //#define PIN_P0_02                        P0_02  // AUX1 (Interrupt Capable/ADC/Serial Port 0)
 //#define PIN_P0_03                        P0_03  // AUX1 (Interrupt Capable/ADC/Serial Port 0)
-//#define PS_ON_PIN                        P1_23  // SERVO P1.23
+#define PS_ON_PIN                        P2_00  // SERVO P1.23
